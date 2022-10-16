ef2080f0dee20fb7d476e2760960d8bbcd0f6220
mushroom <mushroom019.coder@gmail.com>
2022-10-16 21:28:18 +0530

    finished inbox bug

--- src/modules/time.js
+++ src/modules/time.js
@@ -1,13 +1,24 @@
 import { doneTask } from "./done";
+import { differenceInCalendarDays } from "date-fns/fp"
 export default function time(array){
     let inbox=[]
     const today=new Date();
-    array.forEach(element => {
-        let newDate=new Date(element.date)
-        if (newDate<today){
+    const time=`${today.getHours()}:${today.getMinutes()}`
+    let todayArray=[]
+    array.forEach(task => {
+        let newDate = new Date(task.date);
+        const distance = differenceInCalendarDays(newDate, today);
+        if (distance === 0) {
+            todayArray.push(task)
+        }
+    })
+
+    todayArray.forEach(element => {
+        if (element.time<=time){
             inbox.push(element)
         }
     });
     doneTask(inbox)
 
 }
+
