34453dabf50c3995391f3139597b11e33059c19a
mushroom <mushroom019.coder@gmail.com>
2022-10-16 19:06:07 +0530

    finished the project

--- src/modules/time.js
+++ src/modules/time.js
@@ -1,17 +1,14 @@
-import showTask from "./showTask";
-
+import { doneTask } from "./done";
 export default function time(array){
     let inbox=[]
     const today=new Date();
     const time=`${today.getHours()}:${today.getMinutes()}`
-    console.log (time)
 
     array.forEach(element => {
-        console.log(element.time)
         if (element.time<=time){
             inbox.push(element)
         }
     });
-    showTask.displayTask(inbox)
+    doneTask(inbox)
 
 }
